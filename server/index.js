1f38e61358b9c3274fc8c69545c72108bd5bf513
Narendra <alenway97@gmail.com>
2023-02-16 18:23:06 +0530

    connected to the mongodb server

--- server/index.js
+++ server/index.js
@@ -2,3 +2,22 @@ import bodyParser from "body-parser";
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+//https://www.mongodb.com
+const CONNECTION_URL =
+  "mongodb+srv://javascriptmemoryproject:javascriptmemoryproject123@cluster0.qjnmqhw.mongodb.net/?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server in working on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
