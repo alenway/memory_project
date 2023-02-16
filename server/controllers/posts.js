ffeda298ce4ebcd3a5f55f7c311a782c2c329447
Narendra <alenway97@gmail.com>
2023-02-16 20:41:28 +0530

    created post functions

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,5 +1,22 @@
-const getPosts = (req, res) => {
-  res.send("<h1>This works</h1>");
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+    console.log(postMessages);
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 };
 
-export default getPosts;
+export const createPost = async (req, res) => {
+  const post = req.body;
+  const newPost = new PostMessage(post);
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
